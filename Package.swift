5708e6170dbcd2e37fb0654371d8945384caf640
Minsoo Kim <vibrio0102@gmail.com>
2022-03-18 17:23:03 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Cosmos",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
+    ],
     products: [
         .library(name: "Cosmos", targets: ["Cosmos"]),
     ],
